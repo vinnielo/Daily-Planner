70b1798bae20af50e83e187c3f38b0af4fe723df
jose Lopez <vinnielo01@gmail.com>
2020-04-30 10:03:00 -0700

    updated local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,56 @@
-console.log("script file is laoded!");
+var planner = localStorage.getItem('planner')? JSON.parse(localStorage.getItem("planner")) : {};
+console.log(planner)
 //add ready event listener to the document
-$(document).ready(function() {
-    console.log("document is ready to go!");
-    //render the current time
-    var getCurrentTime = function() {
-        var currentTime = moment().format('LLLL');
-        $('#currentDay').text(currentTime);
+$(document).ready(function () {
+  console.log("document is ready to go!");
+  //render the current time
+  var getCurrentTime = function () {
+    var currentTime = moment().format("LLLL");
+    $("#currentDay").text(currentTime);
+  };
+
+  //update the hours div with past, present future classes
+  var setTimeColor = function () {
+    //get current time
+    var getCurrentHourValue = moment().hours();
+    //get all div
+    var $allTimeBlocksArr = $(".row.time-block");
+    //check current time
+    for (var i = 0; i < $allTimeBlocksArr.length; i++) {
+      var $targetBlock = $($allTimeBlocksArr[i]);
+      var timeAttr = $targetBlock.attr("time");
+      //compare time attr with current hour value
+      if (getCurrentHourValue > timeAttr) {
+        $targetBlock.find(".description").addClass("past");
+      } else if (getCurrentHourValue < timeAttr) {
+        $targetBlock.find(".description").addClass("future");
+      } else if (getCurrentHourValue === timeAttr) {
+        $targetBlock.find(".description").addClass("present");
+      }
+      var txt = document.getElementById(`hour-${i+9}`);
+      txt.childNodes[3].textContent = planner[`hour-${i+9}`];
+
     }
+  };
+  //local storage button function
+  //addEventListener
+  $(".saveBtn").on("click", function () {
+    event.preventDefault();
+    var parent = this.parentNode;
+    var txt = parent.childNodes[3];
+    var plannerText = txt.value
+    var key = parent.id
+    planner[key] = plannerText
+    localStorage.setItem("planner", JSON.stringify(planner))
 
-    //update the hours div with past, present future classes
-    var setTimeColor = function() {
-        //get current time
-        var getCurrentHourValue = moment().hours();
-        //get all div
-        var $allTimeBlocksArr = $(".row.time-block");
-        //check current time
-        for (var i = 0; i < $allTimeBlocksArr.length; i++) {
-            var $targetBlock = $($allTimeBlocksArr[i]);
-            var timeAttr = $targetBlock.attr('time');
-            //compare time attr with current hour value
-            if (getCurrentHourValue > timeAttr) {
-                $targetBlock.find(".description").addClass('past');
-            } else if (getCurrentHourValue < timeAttr) {
-                $targetBlock.find(".description").addClass('future');
-            } else if (getCurrentHourValue === timeAttr) {
-                $targetBlock.find(".description").addClass('present');
-            }
-        }
-    }
+  });
+  //get textarea value //event.target .closest() .find()
+  //parse in JSON format
+  //setItem into local storage
 
-    //local storage button function
-        //addEventListener
-        //get textarea value //event.target .closest() .find()
-        //parse in JSON format
-        //setItem into local storage
+  //get localstorage item then populate page
 
-    //get localstorage item then populate page
-
-    //run the functions
-    getCurrentTime();
-    setTimeColor();
-
+  //run the functions
+  getCurrentTime();
+  setTimeColor();
 });
